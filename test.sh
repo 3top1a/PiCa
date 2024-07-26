ca4a1a84f44cde17ae47b4d21ba04fcee720613f
3top1a <3top1a.official@gmail.com>
2024-07-26 21:49:06 +0200

    added cutechess bash script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir testout
+id=$(date +%H:%M-%d.%m)
+
+cutechess-cli \
+-engine conf="pica" \
+-engine conf="pica-old" \
+-each \
+    tc=inf/10+0.1 \
+-games 2 -rounds 2500 -repeat 2 -maxmoves 200 \
+-openings file=silversuite.pgn plies=20 \
+-sprt elo0=0 elo1=10 alpha=0.05 beta=0.05 \
+-concurrency 6 \
+-ratinginterval 10 \
+-recover \
+-pgnout "testout/sprt-${id}.pgn"
